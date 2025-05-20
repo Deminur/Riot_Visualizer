962eb0e5e843095d4601ef70beb612cd14dcc3d8
DELMAS Théo <theo_delmas@orange.fr>
2025-05-20 16:37:57 +0200

    fix: player not found scenario now handeled

--- jfx-frontend/src/main/java/front/ThreadableOnClickSearch.java
+++ jfx-frontend/src/main/java/front/ThreadableOnClickSearch.java
@@ -38,7 +38,7 @@ public class ThreadableOnClickSearch implements Runnable {
             JSONObject data = (JSONObject) parser.parse(inline);
             this.puuid = ((String) data.get("puuid"));
             String pseudo = (String) data.get("gameName");
-            if(this.puuid!="??"){
+            if(!this.puuid.equals("??")){
                 //Get ID
                 url = new URL("http://localhost:"+PORT+"/getID/?puuid="+this.puuid);
                 inline = "";
@@ -80,9 +80,16 @@ public class ThreadableOnClickSearch implements Runnable {
                 playerStats.setFlexStats(flexStats);
                 this.parent.setPlayerStats(playerStats);
 
-                //refresh the Frontend
-                Platform.runLater(()->this.parent.refreshIHM());
+            }else{//player not found
+                PlayerStats playerStats = new PlayerStats("??", "??");
+                PlayerRankedStats soloqueue = new PlayerRankedStats("??", 0, 0);
+                playerStats.setSoloqueueStats(soloqueue);
+                PlayerRankedStats flex = new PlayerRankedStats("??", 0, 0);
+                playerStats.setFlexStats(flex);
+                this.parent.setPlayerStats(playerStats);
             }
+            //refresh the Frontend
+            Platform.runLater(()->this.parent.refreshIHM());
         } catch (Exception e) {
             // TODO: handle exception
             System.out.println(e);
